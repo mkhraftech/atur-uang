2e9cbbb428d20f57e1802e466b54b1e43f9a203d
rafiq <muhkhr-tech@gmail.com>
2026-04-22 16:15:05 +0700

    connect to neon db

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5436/belanja-db"
+DATABASE_URL = "postgresql://neondb_owner:npg_O0t5iMeBhlHx@ep-royal-credit-ao4ehqt4.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
