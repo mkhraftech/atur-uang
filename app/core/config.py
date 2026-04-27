9817c9cf750f67abf7ec406ebb7bea0408610c47
rafiq <muhkhr-tech@gmail.com>
2026-04-28 03:57:56 +0700

    fix: comment out unused GOOGLE_REDIRECT_URI

--- app/core/config.py
+++ app/core/config.py
@@ -30,7 +30,7 @@ class Settings(BaseSettings):
     # GOOGLE OAUTH2
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
-    GOOGLE_REDIRECT_URI: str
+    # GOOGLE_REDIRECT_URI: str
     BASE_URL: str
 
 
